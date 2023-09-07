67494ec092b6e49cfe069c527ebe5c03ce538bd8
Qamar Tanweer <qtanweer.mts41ceme@gmail.com>
2023-09-07 11:13:10 +0000

    more coomon utils

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -66,6 +66,18 @@ def save_json(path: Path, data: dict):
     with open(path, "w", encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4)
     logger.info("json file: %s saved successfully", path)
+    
+@ensure_annotations
+def save_binary(path: Path, data: Any) -> None:
+    """
+    Saves a binary file
+
+    Args:
+        path (Path): path to binary file
+        data (Any): data to be saved in binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info("binary file: %s saved successfully", path)
 
 @ensure_annotations
 def load_bin(path: Path) -> Any:
@@ -97,3 +109,20 @@ def get_size(path: Path) -> Any:
     size = f"~ {round(os.path.getsize(path) / 1024, 2)} KB"
     logger.info("file: %s size: %s", path, size)
     return size
+
+def decode_image(imgstring, file_name):
+    """
+    Decodes image from base64 string
+    """
+    imgdata = base64.b64decode(imgstring)
+    with  open(file_name, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+def encode_image_base64(cropped_image_path):
+    """
+    Encodes image to base64 string
+    """
+    with open(cropped_image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string
