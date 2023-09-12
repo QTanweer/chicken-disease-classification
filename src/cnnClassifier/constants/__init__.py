be2bbb6557043d442a0a6f8c0ee8eb9ea3def32c
Qamar Tanweer <qtanweer.mts41ceme@gmail.com>
2023-09-12 13:08:05 +0000

    fixed params file path

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -4,4 +4,4 @@ CONSTANTS
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("/workspaces/chicken-disease-classification/config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+PARAMS_FILE_PATH = Path("/workspaces/chicken-disease-classification/params.yaml")
