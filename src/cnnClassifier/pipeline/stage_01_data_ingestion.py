b5ae4c195b3bfce2a49cacc9a46e33b4952bbc5f
QTanweer <qtanweer.mts41ceme@gmail.com>
2023-09-14 02:35:06 +0500

    data ingestion pipeline

---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,37 @@
+"""
+Data Ingestion Pipeline Stage 01
+"""
+from cnnClassifier.config.configuration import ConfiguratonManager
+from cnnClassifier.compoents.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    """
+    Data Ingestion Pipeline Stage 01
+    """
+    def __init__(self):
+        pass
+
+    def main(self):
+        '''
+        Main method for Data Ingestion Pipeline Stage 01
+        '''
+        config = ConfiguratonManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config = data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(">>>>>Stage: %s started<<<<<" , STAGE_NAME)
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(">>>>>Stage: %s completed<<<<<" , STAGE_NAME)
+    except Exception as ex:
+        logger.error(ex, exc_info = True)
+        raise ex
+    
