0fdb8db20fdf6e72dc5997bd1339e8a89e517868
QTanweer <qtanweer.mts41ceme@gmail.com>
2023-09-14 02:36:32 +0500

    set entry point to run data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Entry point for the application.
+This module is responsible for calling the main method of the pipeline.
+"""
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(">>>>>Stage: %s started<<<<<" , STAGE_NAME)
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(">>>>>Stage: %s completed<<<<<" , STAGE_NAME)
+except Exception as ex:
+    logger.error(ex, exc_info = True)
+    raise ex
